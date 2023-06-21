b9569dbc0918296ae739b0595aaf16a7f70b7150
Thijmen2006 <169696@student.horizoncollege.nl>
2023-06-21 11:23:52 +0200

    sidebar en footer en images

--- wp-content/themes/ThemeBuilder/index.php
+++ wp-content/themes/ThemeBuilder/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
     <meta charset="UTF-8">
@@ -10,7 +11,7 @@
 
     <!-- De onderstaande regel code gebruikt de WordPress-functie get_stylesheet_uri() om de URL van het huidige thema's style.css-bestand op te halen en dit te koppelen aan het stijlblad van je website. -->
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-    
+
 </head>
 
 <body>
@@ -21,25 +22,27 @@
 
             <!-- De onderstaande regel code gebruikt de WordPress-functie  -->
             <?php
-                 wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
+            wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
             ?>
         </nav>
     </header>
 
     <main>
-    <!-- De onderstaande regels code kijkt of en hoeveel posts er zijn en plaatst deze op de pagina.  -->
-    <?php
+        <?php
         if (have_posts()) :
             while (have_posts()) :
                 the_post();
-                ?>
+        ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <h2 class="entry-title"><a class="posts-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <div class="entry-content">
                         <?php the_content(); ?>
+                        <?php if (has_post_thumbnail()) : ?>
+                            <img src="<?php the_post_thumbnail_url(); ?>" alt="Post Image" width="200">
+                        <?php endif; ?>
                     </div>
                 </article>
-                <?php
+            <?php
             endwhile;
         else :
             ?>
@@ -47,8 +50,38 @@
         <?php endif; ?>
     </main>
 
+
     <footer>
-            <!-- Voeg hier je footerinhoud toe -->
+        <div class="container-footer w-container">
+            <div class="w-row">
+                <div class="footer-column w-clearfix w-col w-col-4"><img src="https://uploads-ssl.webflow.com/5966ea9a9217ca534caf139f/596d33f36607b12cfdaf8ad2_LogoWhite.png" alt="Company image" width="40" class="failory-logo-image">
+                    <h3 class="footer-failory-name">MijnBedrijf</h3>
+                </div>
+                <div class="footer-column w-col w-col-8">
+                    <div class="w-row">
+                        <div class="w-col w-col-8">
+                            <div class="w-row">
+                                <div class="w-col w-col-7 w-col-small-6 w-col-tiny-7">
+                                    <h3 class="footer-titles">Content</h3>
+                                    <p class="footer-links"><a href=""><span class="footer-link">Homepage</span></a><span><br></span><a href="http://localhost/ThemeBuilder/ThemeBuilder/2023/06/15/de-voordelen-van-regelmatige-lichaamsbeweging/"><span class="footer-link">Post 1<br></span></a><a href="http://localhost/ThemeBuilder/ThemeBuilder/2023/06/15/tips-voor-het-opstarten-van-een-succesvolle-blog/"><span class="footer-link">Post 2<br></span></a><strong><br></strong></p>
+                                </div>
+                                <div class="w-col w-col-5 w-col-small-6 w-col-tiny-5">
+                                    <h3 class="footer-titles">Learn</h3>
+                                    <p class="footer-links"><a href="http://localhost/ThemeBuilder/ThemeBuilder/over-ons/"><span class="footer-link">Over Ons<br></span></a><strong><br></strong></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column-center-mobile w-col w-col-4">
+                            <h3 class="footer-titles">Follow Us!</h3><a href="" target="_blank" class="footer-social-network-icons w-inline-block"><img src="https://uploads-ssl.webflow.com/5966ea9a9217ca534caf139f/5c8dbf0a2f2b67e3b3ba079c_Twitter%20Icon.svg" width="20" alt="Twitter icon"></a><a href="" target="_blank" class="footer-social-network-icons w-inline-block"><img src="https://uploads-ssl.webflow.com/5966ea9a9217ca534caf139f/5c8dbfe70fcf5a0514c5b1da_Instagram%20Icon.svg" width="20" alt="Instagram icon"></a><a href="" target="_blank" class="footer-social-network-icons w-inline-block"><img src="https://uploads-ssl.webflow.com/5966ea9a9217ca534caf139f/5c8dbe42e1e6034fdaba46f6_Facebook%20Icon.svg" width="20" alt="Facebook Icon"></a><a href="" target="_blank" class="footer-social-network-icons w-inline-block"><img src="https://uploads-ssl.webflow.com/5966ea9a9217ca534caf139f/5c8dc0002f2b676eb4ba0869_LinkedIn%20Icon.svg" width="20" alt="LinkedIn Icon"></a><a href="" target="_blank" class="footer-social-network-icons w-inline-block"><img src="https://uploads-ssl.webflow.com/5966ea9a9217ca534caf139f/5c8dc0112f2b6739c9ba0871_Pinterest%20Icon.svg" width="20" alt="Pinterest Icon" class=""></a>
+                            <p class="footer-description">Email me at: <a href=""><strong class="link-email-footer">Lorem Ipsum</strong></a><br></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
+
+
 </body>
+
 </html>
